e528aab82714af23865e1827433ce6c5c05393f4
paolo <pvictorsys@gmail.com>
2022-05-16 21:49:00 -0500

    handle error on solana program

--- solana_program/src/lib.rs
+++ solana_program/src/lib.rs
@@ -1,6 +1,6 @@
 use borsh::{BorshSerialize, BorshDeserialize};
 use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey, msg
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey, msg, program_error::ProgramError
 };
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
@@ -15,18 +15,23 @@ fn execute(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
+    let mut data = instruction_data;
 
-    msg!("msg: {:?}", instruction_data);
+    let instr_result: Result<TestInstruction, std::io::Error> = borsh::BorshDeserialize::deserialize(&mut data);
 
-    let mut t = instruction_data;
-
-    let instr: TestInstruction = borsh::BorshDeserialize::deserialize(&mut t).unwrap();
-
-    msg!(
-        "transaction_data: {}: {} accounts, data={:?}",
-        program_id,
-        accounts.len(),
-        instr
-    );
-    Ok(())
+    match instr_result {
+        Ok(instruction) => {
+            msg!(
+                "transaction_data: {}: {} accounts, data={:?}",
+                program_id,
+                accounts.len(),
+                instruction
+            );
+            return Ok(());
+        },
+        Err(error) => {
+            msg!("Error trying to parse instruction: {:?}", error);
+            return Err(ProgramError::InvalidInstructionData);
+        }
+    }
 }
